7bd830f84cc549b5fa6e62dac8aba15b70cd06d4
轩灵 <48324481+fltenwall@users.noreply.github.com>
2024-11-05 18:12:53 +0800

    Update select_number.R

--- select_number.R
+++ select_number.R
@@ -1,62 +1,66 @@
-set.seed(123)
-data <- array(runif(1000,0,1), dim=c(10,10,10)); 
 
-first_level_random = sample(0:10, 1)
-second_level_random = sample(0:10, 1)
-third_level_random = sample(0:10, 1)
-#data[first_level_random_select,second_level_random_select,third_level_random_select]
+data <- array(runif(1000000,0,1), dim=c(100,100,100)); 
+
 count <- 0
 max_count <- 100
 number_count <- 0
-select_arr <- list()
+
 first_level_random_selected <- list()
 second_level_random_selected <- list()
 third_level_random_selected <- list()
+
+selected_data <- array()
+
 repeat{
-  first_level_random = sample(0:10, 1)
-  second_level_random = sample(0:10, 1)
-  third_level_random = sample(0:10, 1)
-  
+  first_level_random <- sample(1:100, 1)
+    second_level_random <- sample(1:10, 1)
+    third_level_random <- sample(1:10, 1)
+
   repeat{
     if(first_level_random %in% first_level_random_selected){
       break
     }else{
       append(first_level_random_selected,first_level_random)
+      break
     }
     
     if(second_level_random %in% second_level_random_selected){
       break
     }else{
       append(second_level_random_selected,second_level_random)
+      break
     }
     
     if(third_level_random %in% third_level_random_selected){
       break
     }else{
      append(third_level_random_selected,third_level_random)
+     break
     }
   }
-  for(second_arr in data[first_level_random,,]){
-    second_arr_mean = mean(second_arr)
-    for(item in second_arr){
-      if(item > second_arr_mean){
-        count = count + 1
-        append(select_arr,item)
+    
+    select_arr <- array(data[first_level_random, second_level_random, ])
+    select_arr_mean <- mean(select_arr)
+
+  for(item in select_arr){
+      if(!is.na(item) && item > select_arr_mean){
+        count <- count + 1
+        selected_data = append(selected_data,item)
+        # selected_data[[length(selected_data)+1]] <- count
       }
       if(count >= max_count){
         break
       }
-    }
-    if(count >= max_count){
-      break
-    }
   }
   if(count >= max_count){
     break
   }    
 }
 
-print(select_arr)
+print(selected_data)
+selected_data_mean <- mean(selected_data)
+print(selected_data_mean)
+
 
 
 
